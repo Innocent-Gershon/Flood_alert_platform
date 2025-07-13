b858b43fa34dc70c72439a71e3f6a50cc55e9de8
INNOCENT NANGAH <nnocentnangahi@0000s-MacBook-Pro.local>
2025-07-13 15:37:07 +0200

    Done

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.use('/api/alerts', require('./routes/alertRoutes'));
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch(err => console.error(err));
